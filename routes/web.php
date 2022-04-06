cf302ac97e544bda970f5baa40cba52424822405
vijoy sasidharan <devuinfosys@gmail.com>
2022-04-06 21:18:02 +0530

    User module updated

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('login');
 });
 
-
 Route::post('/', 'App\Http\Controllers\UserController@login')->name('user.login');
 
 Route::group(['middleware' => ['auth']], function(){
