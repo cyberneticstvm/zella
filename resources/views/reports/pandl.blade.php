33dcec90860140aa3ebe7aa2df5910fda9dff574
CYBER NETICS <cybernetics.me@outlook.com>
2022-04-25 17:18:19 +0530

    small updates

--- resources/views/reports/pandl.blade.php
+++ resources/views/reports/pandl.blade.php
@@ -86,7 +86,9 @@
                                             <td class="text-right">{{ $record->income }}</td>
                                             <td class="text-right">{{ number_format($record->income-$record->expense, 2) }}</td>
                                         </tr>
-                                        {{ $etot += $record->expense }} {{ $itot += $record->income }} {{ $ptot += $record->income-$record->expense }}
+                                        @php 
+                                            $etot += $record->expense; $itot += $record->income; $ptot += $record->income-$record->expense
+                                        @endphp
                                     @endforeach
                                     <tr><td colspan="5" class="text-right fw-bold">Total</td><td class="text-right fw-bold">{{ number_format($etot, 2) }}</td><td class="text-right fw-bold">{{ number_format($itot, 2) }}</td><td class="text-right fw-bold">{{ number_format($ptot, 2) }}</td></tr>
                                     <tr><td colspan="5" class="text-right fw-bold">Expense Total</td><td colspan="3" class="text-right fw-bold">{{ number_format($expenses, 2) }}</td></tr>
