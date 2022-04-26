104a297d6bdbb07ddd10e65025a1df695d90e351
CYBER NETICS <cybernetics.me@outlook.com>
2022-04-26 18:54:20 +0530

    Small Updates

--- resources/views/pdf/products.blade.php
+++ resources/views/pdf/products.blade.php
@@ -22,13 +22,14 @@
         <h5>PRODUCT LIST</h5>
     </center>
     <table width="100%" class="bordered" cellspacing="0" cellpadding="0">
-        <thead><tr><th>SL No.</th><th>Product Name</th><th>Collection Name</th><th>Selling Price</th><th>Description</th></tr>
+        <thead><tr><th>SL No.</th><th>Product Name</th><th>SKU</th><th>Collection Name</th><th>Selling Price</th><th>Description</th></tr>
         <tbody>
             @php $c = 1; @endphp
             @foreach($products as $row)
             <tr>
                 <td>{{ $c++ }}</td>
                 <td>{{ $row->pname }}</td>
+                <td>{{ $row->sku }}</td>
                 <td>{{ $row->cname }}</td>
                 <td>{{ $row->selling_price }}</td>
                 <td>{{ $row->description }}</td>
