a49c73ff3afbfbf6caa360ef8c5d5babfe88b196
CYBER NETICS <cybernetics.me@outlook.com>
2022-04-26 18:51:41 +0530

    Small Updates

--- app/Exports/SalesExport.php
+++ app/Exports/SalesExport.php
@@ -43,7 +43,7 @@ class SalesExport implements FromCollection, WithHeadings
             return $query->where('sd.product', $product);
         })->when(isset($pmode), function($query) use ($request, $pmode){
             return $query->where('s.payment_mode', $pmode);
-        })->when(isset($pstatus), function($query) use ($request){
+        })->when(isset($pstatus), function($query) use ($request, $pstatus){
             return $query->where('s.payment_status', $pstatus);
         })->groupBy('s.id', 's.customer_name', 's.contact_number', 's.address', 's.payment_status', 's.payment_mode', 's.sold_date', 's.discount', 'sd.vat_percentage')->get();
         return $sales;
