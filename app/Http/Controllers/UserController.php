db5467ec087a6a11bf86b2750a9b0a00329a861f
CYBER NETICS <cybernetics.me@outlook.com>
2022-06-02 22:47:12 +0530

    Small Updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,10 +23,10 @@ class UserController extends Controller
 
     function __construct(){
 
-        /*$this->middleware('permission:user-list|user-create|user-edit|user-delete', ['only' => ['index','show']]);
+        $this->middleware('permission:user-list|user-create|user-edit|user-delete', ['only' => ['index','show']]);
         $this->middleware('permission:user-create', ['only' => ['create','store']]);
         $this->middleware('permission:user-edit', ['only' => ['edit','update']]);
-        $this->middleware('permission:user-delete', ['only' => ['destroy']]);*/
+        $this->middleware('permission:user-delete', ['only' => ['destroy']]);
 
         $this->settings = DB::table('settings')->find(1);
         $vat = $this->settings->vat_percentage;
