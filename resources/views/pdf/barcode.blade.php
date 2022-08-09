1177ab14d94a56d5c9b09fb2be309ee2170d4a82
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-08-09 10:50:24 +0530

    small updates

--- resources/views/pdf/barcode.blade.php
+++ resources/views/pdf/barcode.blade.php
@@ -4,6 +4,7 @@
 </head>
 <body>
    <p>Barcode</p>
-   {!! DNS1D::getBarcodeHTML('4445645656', 'C39') !!}
+   {!! DNS1D::getBarcodeHTML($product->sku, 'C39') !!}
+   <img src="data:image/png;base64,{{DNS1D::getBarcodePNG('11', 'C39')}}" alt="barcode" />
 </body>
 </html>
