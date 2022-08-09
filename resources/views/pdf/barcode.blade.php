56a3771a4b4124c0d2ae56d6f3d9d8c83b039409
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-08-09 10:51:56 +0530

    small updates

--- resources/views/pdf/barcode.blade.php
+++ resources/views/pdf/barcode.blade.php
@@ -3,8 +3,7 @@
     <title>Zella Boutique UAE</title>
 </head>
 <body>
-   <p>Barcode</p>
-   {!! DNS1D::getBarcodeHTML($product->sku, 'C39') !!}
-   <img src="data:image/png;base64,{{DNS1D::getBarcodePNG('11', 'C39')}}" alt="barcode" />
+   <!--{!! DNS1D::getBarcodeHTML($product->sku, 'C39') !!}-->
+   <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($product->sku, 'C39')}}" alt="{{ $product->sku }}" />
 </body>
 </html>
