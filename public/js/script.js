7a8b482ab325f10660f6446de0b8ce66de914d77
CYBER NETICS <cybernetics.me@outlook.com>
2022-08-24 08:57:33 +0530

    small updates

--- public/js/script.js
+++ public/js/script.js
@@ -73,7 +73,8 @@ $(function(){
     });
 
     $(".addPurchaseRow").click(function(){
-        $(".tblPurchase tbody").append("<tr><td><select class='form-control form-control-md select2 selProduct' name='product[]' required='required'><option value=''>Select</option></select></td><td><input type='number' class='form-control text-right qty' placeholder='0' step='any' name='qty[]' required='required'></td><td><input type='number' step='any' class='form-control text-right price' placeholder='0.00' name='price[]' required='required'></td><td><input type='number' step='any' class='form-control text-right total' placeholder='0.00' name='total[]' required='required'></td><td class='text-center'><a href='javascript:void(0)' onClick='$(this).parent().parent().remove()'><i class='fa fa-trash text-danger'></i></a></td></tr>");
+        var pmode = $(".payment_mode").val();
+        $(".tblPurchase tbody").append("<tr><td><select class='form-control form-control-md select2 selProduct' name='product[]' required='required'><option value=''>Select</option></select></td><td><input type='number' class='form-control text-right qty' placeholder='0' step='any' name='qty[]' required='required'></td><td><input type='number' step='any' class='form-control text-right price' placeholder='0.00' name='price[]' required='required'></td><td><input type='number' step='any' class='form-control text-right total' placeholder='0.00' name='total[]' required='required'></td><td class='text-center'><a href='javascript:void(0)' onClick='calculateTotal("+pmode+");$(this).parent().parent().remove()'><i class='fa fa-trash text-danger'></i></a></td></tr>");
         $('.selProduct').select2();
         bindDDL('product', 'selProduct');
     })
