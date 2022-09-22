54c72672594da4ae3869ad177d2cc1f0995146c7
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-09-22 22:09:44 +0530

    small updates

--- resources/views/barcode.blade.php
+++ resources/views/barcode.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <small>{{ $product->name }}</small><br/>    
-    <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($product->sku, 'C128')}}" alt="{{ $product->sku }}" width="144" height="95" /><br/>
+    <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($product->sku, 'C128')}}" alt="{{ $product->sku }}" width="500" height="95" /><br/>
     <small>AED: {{ $product->selling_price }}</small>
 </body>
 </html>
