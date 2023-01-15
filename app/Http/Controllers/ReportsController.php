dd3fc43bac78298a3f184314bb661628f1b9631d
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-01-15 09:04:03 +0530

    small updates

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -248,11 +248,11 @@ class ReportsController extends Controller
     }
 
     public function dayBook(){
-        $sales_cash = DB::table('sales AS s')->selectRaw("CASE WHEN cash_collected > 0 THEN cash_collected ELSE order_total END AS order_total")->whereIn('payment_mode', ['cash','candc'])->whereDate('s.sold_date', Carbon::today())->get()->sum('order_total');
-        $sales_card = DB::table('sales AS s')->selectRaw("CASE WHEN card_collected > 0 THEN card_collected ELSE order_total END AS order_total")->whereIn('payment_mode', ['card','candc'])->whereDate('s.sold_date', Carbon::today())->get()->sum('order_total');
-        $purchases = DB::table('purchases as p')->whereDate('p.delivery_date', Carbon::today())->get()->sum(DB::raw("p.purchase_total+p.other_expense"));
-        $expenses = DB::table('expenses')->where('head', '!=', 15)->whereDate('date', Carbon::today())->get()->sum('amount');
-        $incomes = DB::table('incomes')->where('head', '!=', 16)->whereDate('date', Carbon::today())->get()->sum('amount');
+        $sales_cash = DB::table('sales AS s')->selectRaw("CASE WHEN cash_collected > 0 THEN cash_collected ELSE order_total END AS order_total")->whereIn('payment_mode', ['cash','candc'])->whereDate('s.sold_date', '2023-01-10')->get()->sum('order_total');
+        $sales_card = DB::table('sales AS s')->selectRaw("CASE WHEN card_collected > 0 THEN card_collected ELSE order_total END AS order_total")->whereIn('payment_mode', ['card','candc'])->whereDate('s.sold_date', '2023-01-10')->get()->sum('order_total');
+        $purchases = DB::table('purchases as p')->whereDate('p.delivery_date', '2023-01-10')->get()->sum(DB::raw("p.purchase_total+p.other_expense"));
+        $expenses = DB::table('expenses')->where('head', '!=', 15)->whereDate('date', '2023-01-10')->get()->sum('amount');
+        $incomes = DB::table('incomes')->where('head', '!=', 16)->whereDate('date', '2023-01-10')->get()->sum('amount');
         return view('reports.daybook', compact('sales_cash', 'sales_card', 'purchases', 'expenses', 'incomes'));
     }
 
